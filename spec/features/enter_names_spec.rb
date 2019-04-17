e69613bcf461730e8fef4720385c2b4babb4c6a0
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-04-17 18:10:29 +0100

    added web-helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :p1_name, with: 'Callum'
-    fill_in :p2_name, with: 'Stuart'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Callum vs. Stuart'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Hit Points' do
   scenario 'displaying hit points' do
-    visit('/')
-    fill_in :p1_name, with: 'Callum'
-    fill_in :p2_name, with: 'Stuart'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Callum: 100hp'
   end
 end
